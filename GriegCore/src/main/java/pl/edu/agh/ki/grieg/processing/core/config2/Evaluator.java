89d8dbda32cb7dd5f41d8d005537965db1a531f2
los <losiu99@gazeta.pl>
2013-08-18 20:39:23 +0200

    Working on the evaluator

---
+++ GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/core/config2/Evaluator.java
@@ -0,0 +1,71 @@
+package pl.edu.agh.ki.grieg.processing.core.config2;
+
+import com.google.common.reflect.TypeToken;
+
+import pl.edu.agh.ki.grieg.processing.core.config.ConfigException;
+import pl.edu.agh.ki.grieg.processing.core.config2.tree.CompleteValueNode;
+import pl.edu.agh.ki.grieg.processing.core.config2.tree.ComplexValueNode;
+import pl.edu.agh.ki.grieg.processing.core.config2.tree.ConvertibleValueNode;
+import pl.edu.agh.ki.grieg.processing.core.config2.tree.PrimitiveValueNode;
+import pl.edu.agh.ki.grieg.util.Reflection;
+import pl.edu.agh.ki.grieg.util.ReflectionException;
+import pl.edu.agh.ki.grieg.util.converters.ConversionException;
+import pl.edu.agh.ki.grieg.util.converters.Converter;
+
+public class Evaluator implements ValueVisitor {
+
+    private Object value;
+
+    private final Converter converter;
+
+    public Evaluator(Converter converter) {
+        this.converter = converter;
+    }
+
+    public <T> T getValue(Class<T> clazz) {
+        Class<T> unwrapped = Reflection.wrap(clazz);
+        return unwrapped.cast(value);
+    }
+    
+    public Object getValue() {
+        return value;
+    }
+
+    @Override
+    public void visit(CompleteValueNode node) throws ConfigException {
+        value = node.getValue();
+    }
+
+    @Override
+    public void visit(ComplexValueNode<?> node) throws ConfigException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visit(ConvertibleValueNode node) throws ConfigException {
+        String type = node.getType();
+        String literal = node.getValue();
+        try {
+            Class<?> clazz = Reflection.getClass(type);
+            Class<?> unwrapped = Reflection.wrap(clazz);
+            value = converter.convert(literal, TypeToken.of(unwrapped));
+        } catch (ReflectionException e) {
+            throw new ConfigException("Invalid type", e);
+        } catch (ConversionException e) {
+            throw new ConfigException("Conversion failure", e);
+        }
+    }
+
+    @Override
+    public void visit(PrimitiveValueNode node) throws ConfigException {
+        try {
+            Class<?> type = Reflection.wrap(node.getType());
+            String literal = node.getValue();
+            value = converter.convert(literal, TypeToken.of(type));
+        } catch (ConversionException e) {
+            throw new ConfigException("Conversion failure", e);
+        }
+    }
+
+}
---
+++ GriegCore/src/test/java/pl/edu/agh/ki/grieg/processing/core/config2/EvaluatorTest.java
@@ -0,0 +1,119 @@
+package pl.edu.agh.ki.grieg.processing.core.config2;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.reflect.TypeToken;
+
+import pl.edu.agh.ki.grieg.processing.core.config.ConfigException;
+import pl.edu.agh.ki.grieg.processing.core.config2.tree.CompleteValueNode;
+import pl.edu.agh.ki.grieg.processing.core.config2.tree.ConvertibleValueNode;
+import pl.edu.agh.ki.grieg.processing.core.config2.tree.PrimitiveValueNode;
+import pl.edu.agh.ki.grieg.processing.core.config2.tree.ValueNode;
+import pl.edu.agh.ki.grieg.util.converters.ConversionException;
+import pl.edu.agh.ki.grieg.util.converters.Converter;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EvaluatorTest {
+    
+    @Mock private Converter converter;
+
+    @InjectMocks private Evaluator evaluator;
+
+    @Test
+    public void canEvalPrimitiveChar() throws Exception {
+        ValueNode node = new PrimitiveValueNode("  $", char.class);
+        when(converter.convert(eq("  $"), any(TypeToken.class)))
+                .thenReturn('$');
+        node.accept(evaluator);
+        assertEquals('$', (char) evaluator.getValue(char.class));
+    }
+    
+    @Test
+    public void canEvalPrimitiveByte() throws Exception {
+        ValueNode node = new PrimitiveValueNode(" 16", byte.class);
+        when(converter.convert(eq(" 16"), any(TypeToken.class)))
+                .thenReturn((byte) 16);
+        node.accept(evaluator);
+        assertEquals((byte) 16, (byte) evaluator.getValue(byte.class));
+    }
+    
+    @Test
+    public void canEvalPrimitiveShort() throws Exception {
+        ValueNode node = new PrimitiveValueNode(" 16", short.class);
+        when(converter.convert(eq(" 16"), any(TypeToken.class)))
+                .thenReturn((short) 16);
+        node.accept(evaluator);
+        assertEquals((short) 16, (short) evaluator.getValue(short.class));
+    }
+    
+    @Test
+    public void canEvalPrimitiveInt() throws Exception {
+        ValueNode node = new PrimitiveValueNode(" -16", int.class);
+        when(converter.convert(eq(" -16"), any(TypeToken.class)))
+                .thenReturn(-16);
+        node.accept(evaluator);
+        assertEquals(-16, (int) evaluator.getValue(int.class));
+    }
+    
+    @Test
+    public void canEvalPrimitiveLong() throws Exception {
+        ValueNode node = new PrimitiveValueNode("1234", long.class);
+        when(converter.convert(eq("1234"), any(TypeToken.class)))
+                .thenReturn(1234L);
+        node.accept(evaluator);
+        assertEquals(1234L, (long) evaluator.getValue(long.class));
+    }
+    
+    @Test
+    public void canEvalPrimitiveFloat() throws Exception {
+        ValueNode node = new PrimitiveValueNode("12.34", float.class);
+        when(converter.convert(eq("12.34"), any(TypeToken.class)))
+                .thenReturn(12.34f);
+        node.accept(evaluator);
+        assertEquals(12.34f, (float) evaluator.getValue(float.class), 1e-6f);
+    }
+    
+    @Test
+    public void canEvalPrimitiveDouble() throws Exception {
+        ValueNode node = new PrimitiveValueNode("12.34", double.class);
+        when(converter.convert(eq("12.34"), any(TypeToken.class)))
+                .thenReturn(12.34);
+        node.accept(evaluator);
+        assertEquals(12.34, (double) evaluator.getValue(double.class), 1e-6);
+    }
+    
+    @Test(expected = ConfigException.class)
+    public void failsWhenConversionFails() throws Exception {
+        ValueNode node = new PrimitiveValueNode("12.34", double.class);
+        when(converter.convert(eq("12.34"), any(TypeToken.class)))
+                .thenThrow(new ConversionException());
+        node.accept(evaluator);
+    }
+    
+    @Test
+    public void canEvalCompleteValueNode() throws Exception {
+        Object o = new Object();
+        ValueNode node = new CompleteValueNode(o);
+        node.accept(evaluator);
+        assertSame(o, evaluator.getValue());
+    }
+    
+    @Test
+    public void canEvalConvertibleNode() throws Exception {
+        ValueNode node = new ConvertibleValueNode("file:sth", "java.io.File");
+        when(converter.convert(eq("file:sth"), eq(TypeToken.of(File.class))))
+                .thenReturn(new File("/some/path"));
+        node.accept(evaluator);
+        assertEquals(new File("/some/path"), evaluator.getValue());
+    }
+
+}
