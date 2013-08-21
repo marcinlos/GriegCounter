b2733d0c67b6dd8aab7daab6c2d13de503f040ce
los <losiu99@gazeta.pl>
2013-08-22 00:35:48 +0200

    Extracted base class form XmlBootstrap.

---
+++ GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/core/config/ConfigBasedBootstrap.java
@@ -0,0 +1,95 @@
+package pl.edu.agh.ki.grieg.processing.core.config;
+
+import pl.edu.agh.ki.grieg.io.FileLoader;
+import pl.edu.agh.ki.grieg.processing.core.AbstractBootstrap;
+import pl.edu.agh.ki.grieg.processing.core.Bootstrap;
+import pl.edu.agh.ki.grieg.processing.core.PipelineAssembler;
+import pl.edu.agh.ki.grieg.processing.core.config.evaluator.Config;
+import pl.edu.agh.ki.grieg.processing.core.config.evaluator.ConfigEvaluator;
+import pl.edu.agh.ki.grieg.processing.core.config.evaluator.ConfigEvaluatorBuilder;
+import pl.edu.agh.ki.grieg.processing.core.config.tree.ConfigNode;
+import pl.edu.agh.ki.grieg.util.Properties;
+
+/**
+ * Abstract base class for {@link Bootstrap} implementations using standard
+ * configuration structure described in the
+ * {@link pl.edu.agh.ki.grieg.processing.core.config.tree} package. This class
+ * handles details of config tree evaluation and transforming results into more
+ * usable form.
+ * 
+ * @author los
+ */
+public class ConfigBasedBootstrap extends AbstractBootstrap {
+
+    /** Config object */
+    private Config config;
+
+    /**
+     * For use of subclasses, to avoid problems with resource cleanup when the
+     * ctor throws.
+     */
+    protected ConfigBasedBootstrap() {
+        // empty
+    }
+
+    /**
+     * Creates fully parsed and processed config from the config tree rooted at
+     * {@code node}.
+     * 
+     * @param node
+     *            Root of the config tree
+     * @return Config object
+     * @throws ConfigException
+     *             If there was an error during config processing
+     */
+    protected Config buildConfig(ConfigNode node) throws ConfigException {
+        logger().debug("Creating config evaluator");
+        ConfigEvaluator evaluator = new ConfigEvaluatorBuilder()
+                .build();
+
+        logger().debug("Evaluating configuration...");
+        Config config = evaluator.evaluate(node);
+        logger().debug("Configuration evaluated");
+        return config;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected final void prepare() throws ConfigException {
+        logger().debug("Interpreting configuration");
+
+        chooseFileLoader();
+        loadProperties();
+        buildAssembler();
+    }
+
+    /**
+     * Creates the file loader and registers it in the {@link AbstractBootstrap}
+     */
+    private void chooseFileLoader() {
+        logger().info("Using default file loader");
+        setLoader(new FileLoader());
+    }
+
+    /**
+     * Reads and interprets the properties
+     */
+    private void loadProperties() throws ConfigException {
+        logger().trace("Interpreting properties");
+        Properties properties = config.buildProperties();
+        setProperties(properties);
+    }
+
+    /**
+     * Creates the pipeline assembler specified by the configuration
+     */
+    private void buildAssembler() throws ConfigException {
+        logger().info("Searching for pipeline assembler");
+        PipelineAssembler assembler = config.createAssembler();
+        logger().info("Created pipeline assembler");
+        setPipelineAssembler(assembler);
+    }
+
+}
--- GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/core/config/XmlBootstrap.java
+++ GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/core/config/XmlBootstrap.java
@@ -8,46 +8,39 @@ import java.net.URL;
 
 import org.w3c.dom.Document;
 
-import pl.edu.agh.ki.grieg.io.FileLoader;
-import pl.edu.agh.ki.grieg.processing.core.AbstractBootstrap;
-import pl.edu.agh.ki.grieg.processing.core.PipelineAssembler;
 import pl.edu.agh.ki.grieg.processing.core.config.evaluator.Config;
-import pl.edu.agh.ki.grieg.processing.core.config.evaluator.ConfigEvaluator;
-import pl.edu.agh.ki.grieg.processing.core.config.evaluator.ConfigEvaluatorBuilder;
+import pl.edu.agh.ki.grieg.processing.core.config.evaluator.XmlConfigException;
 import pl.edu.agh.ki.grieg.processing.core.config.tree.ConfigNode;
 import pl.edu.agh.ki.grieg.processing.core.config.xml.ConfigTreeReader;
 import pl.edu.agh.ki.grieg.processing.core.config.xml.PipelineAssemblerReader;
 import pl.edu.agh.ki.grieg.processing.core.config.xml.PipelineElementReader;
 import pl.edu.agh.ki.grieg.processing.core.config.xml.PipelineReader;
 import pl.edu.agh.ki.grieg.processing.core.config.xml.PropertyReader;
-import pl.edu.agh.ki.grieg.util.Properties;
 import pl.edu.agh.ki.grieg.util.Resources;
 import pl.edu.agh.ki.grieg.util.xml.XmlException;
 import pl.edu.agh.ki.grieg.util.xml.XmlParser;
 import pl.edu.agh.ki.grieg.util.xml.XmlParserBuilder;
+import pl.edu.agh.ki.grieg.util.xml.XmlSchemaNotFoundException;
 import pl.edu.agh.ki.grieg.util.xml.dom.DomConverter;
 import pl.edu.agh.ki.grieg.util.xml.dom.Element;
 
 import com.google.common.io.Closeables;
 
 /**
- * Base class for XML-based bootstrap implementation. Handles actual
- * configuration interpretation and dependency creation, derived classes need
- * only provide the configuration content. Can well be used on its own, by the
- * constructor accepting an {@link InputStream} from which to read the config.
+ * Base class for XML-based bootstrap implementation. Handles XML files parsing
+ * and delegates configuration interpretation and dependency creation to the
+ * parent. Can well be used on its own, by the constructor accepting an
+ * {@link InputStream} from which to read the config.
  * 
  * <p>
  * Uses {@code config.xsd} schema from the classpath for config validation.
  * 
  * @author los
  */
-public class XmlBootstrap extends AbstractBootstrap {
+public class XmlBootstrap extends ConfigBasedBootstrap {
 
     /** Classpath-relative schema path */
     private static final String SCHEMA = "config.xsd";
-
-    /** Config object */
-    private Config config;
 
     /**
      * Initializes the bootstrap objet using specified {@link InputStream} as
@@ -88,14 +81,19 @@ public class XmlBootstrap extends AbstractBootstrap {
      * @throws ConfigException
      *             If the configuration cannot be read or interpreted
      */
-    protected void init(InputStream input) throws ConfigException {
+    protected Config init(InputStream input) throws ConfigException {
+        ConfigNode node = readConfigTree(input);
+        return buildConfig(node);
+    }
+
+    private ConfigNode readConfigTree(InputStream input) throws ConfigException {
         try {
             logger().debug("Configuring XML parser");
-            
+
             logger().info("Loading schema file {} from the classpath", SCHEMA);
             URL url = Resources.get(SCHEMA);
             logger().trace("Schema url: {}", url);
-            
+
             XmlParser parser = new XmlParserBuilder()
                     .useSchema(url)
                     .create();
@@ -106,66 +104,21 @@ public class XmlBootstrap extends AbstractBootstrap {
             logger().debug("Converting DOM tree to simpler form");
             Element root = new DomConverter().convert(doc);
             ConfigTreeReader reader = createConfigReader();
-            ConfigNode node = reader.read(root, null);
-
-            logger().debug("Creating config evaluator");
-            ConfigEvaluator evaluator = new ConfigEvaluatorBuilder()
-                    .build();
-
-            logger().debug("Evaluating configuration...");
-            config = evaluator.evaluate(node);
-            logger().debug("Configuration evaluated");
-        } catch (XmlException e) {
+            return reader.read(root, null);
+        } catch (XmlSchemaNotFoundException e) {
             throw new ResourceNotFoundException(SCHEMA, e);
+        } catch (XmlException e) {
+            throw new XmlConfigException("Error while parsing XML config", e);
         }
     }
 
-    public ConfigTreeReader createConfigReader() {
+    private ConfigTreeReader createConfigReader() {
         logger().debug("Creating XML confiuration tree reader");
         PropertyReader propertyReader = new PropertyReader();
         PipelineReader pipelineReader = new PipelineReader(
                 new PipelineElementReader(),
                 new PipelineAssemblerReader());
         return new ConfigTreeReader(propertyReader, pipelineReader);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void prepare() throws ConfigException {
-        logger().debug("Interpreting configuration");
-
-        chooseFileLoader();
-        loadProperties();
-        buildAssembler();
-    }
-
-    /**
-     * Creates the file loader and registers it in the {@link AbstractBootstrap}
-     */
-    private void chooseFileLoader() {
-        logger().info("Using default file loader");
-        setLoader(new FileLoader());
-    }
-
-    /**
-     * Reads and interprets the properties
-     */
-    private void loadProperties() throws ConfigException {
-        logger().trace("Interpreting properties");
-        Properties properties = config.buildProperties();
-        setProperties(properties);
-    }
-
-    /**
-     * Creates the pipeline assembler specified by the configuration
-     */
-    private void buildAssembler() throws ConfigException {
-        logger().info("Searching for pipeline assembler");
-        PipelineAssembler assembler = config.createAssembler();
-        logger().info("Created pipeline assembler");
-        setPipelineAssembler(assembler);
     }
 
 }
