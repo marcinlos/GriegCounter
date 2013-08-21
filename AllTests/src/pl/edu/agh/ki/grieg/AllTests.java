ccf86aff9265a0c6bf15d38e5bf2359e64038bff
los <losiu99@gazeta.pl>
2013-08-22 01:12:36 +0200

    Added dummy project to use classpath suite to run all the tests at once and collect full coverage report.

---
+++ AllTests/src/pl/edu/agh/ki/grieg/AllTests.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.ki.grieg;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.extensions.cpsuite.ClasspathSuite.ClassnameFilters;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+@ClassnameFilters({".*Test"})
+public class AllTests {
+
+}
