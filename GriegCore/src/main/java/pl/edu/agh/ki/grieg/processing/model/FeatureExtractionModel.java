ac051c6ce2a570ba613db0c18a659d21ec420aba
los <losiu99@gazeta.pl>
2013-08-22 21:18:56 +0200

    Cleaned up FeatureExtractionModel

--- GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/model/FeatureExtractionModel.java
+++ GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/model/FeatureExtractionModel.java
@@ -9,18 +9,18 @@ import pl.edu.agh.ki.grieg.features.FeaturesListener;
 import pl.edu.agh.ki.grieg.model.Model;
 import pl.edu.agh.ki.grieg.model.Models;
 import pl.edu.agh.ki.grieg.model.SimpleModel;
-import pl.edu.agh.ki.grieg.util.ProgressListener;
+import pl.edu.agh.ki.grieg.util.ProgressAdapter;
 
-public class FeatureExtractionModel implements FeaturesListener,
-        ProgressListener {
+public class FeatureExtractionModel extends ProgressAdapter implements
+        FeaturesListener {
 
     private static final Logger logger = LoggerFactory
             .getLogger(FeatureExtractionModel.class);
 
     private final SimpleModel<Float> model = Models.simple(0.0f);
-    
+
     private final ActionFilter filter;
-    
+
     public FeatureExtractionModel(TimeUnit unit, long delta) {
         filter = new FixedRateActionFilter(unit, delta);
     }
@@ -31,25 +31,10 @@ public class FeatureExtractionModel implements FeaturesListener,
     }
 
     @Override
-    public void started() {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
     public void progress(float percent) {
         if (filter.perform()) {
             model.update(percent);
         }
-    }
-
-    @Override
-    public void finished() {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void failed(Exception e) {
-        // TODO Auto-generated method stub
     }
 
     public Model<Float> getModel() {
