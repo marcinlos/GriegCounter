cf1c0cdf16142468df2e51c14fba5e1f8581bec6
los <losiu99@gazeta.pl>
2013-08-22 21:01:32 +0200

    Added some documentation for ActionFilters

--- GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/model/ActionFilter.java
+++ GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/model/ActionFilter.java
@@ -2,8 +2,18 @@ package pl.edu.agh.ki.grieg.processing.model;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * Utility class for performing certain action only when the condition specified
+ * by abstract method {@link #shouldTrigger()} is fulfilled. Condition is
+ * specified by subclassing, or using one of predefined subclasses.
+ * 
+ * @author los
+ */
 public abstract class ActionFilter {
 
+    /**
+     * Empty runnable, not doing anything.
+     */
     private static final Runnable EMPTY = new Runnable() {
         @Override
         public void run() {
@@ -11,18 +21,47 @@ public abstract class ActionFilter {
         }
     };
 
-    protected abstract boolean shouldTrigger();
-
+    /** Wrapped action */
     private final Runnable action;
 
+    /**
+     * Creates new {@link ActionFilter} wrapping no action. This can be useful
+     * if the action is complex (e.g. has parameters) so that it's difficult to
+     * express as {@link Runnable}. Then, user can still benefir from the
+     * condition and state tracking and use boolean value returned by
+     * {@link #perform()}
+     */
     protected ActionFilter() {
         this.action = EMPTY;
     }
 
+    /**
+     * Creates new {@link ActionFilter} wrapping specified action.
+     * 
+     * @param action
+     *            Action to wrap
+     */
     protected ActionFilter(Runnable action) {
         this.action = checkNotNull(action);
     }
 
+    /**
+     * Condition that must be fulfilled if the action is to be performed. It may
+     * have side-effects - this class guarantees it shall be called once per
+     * each {@link #perform()} invocation.
+     * 
+     * @return {@code true} if the action can be performed, {@code false}
+     *         otherwise
+     */
+    protected abstract boolean shouldTrigger();
+
+    /**
+     * Performs wrapped action if the condition specified by
+     * {@link #shouldTrigger()} is fulfilled.
+     * 
+     * @return {@code true} if the action was indeed performed, {@code false}
+     *         otherwise
+     */
     public final boolean perform() {
         final boolean doIt = shouldTrigger();
         if (doIt) {
@@ -31,12 +70,24 @@ public abstract class ActionFilter {
         return doIt;
     }
 
-    protected void doPerform() {
-        action.run();
-    }
-
+    /**
+     * Performs wrapped action without checking the condition specified by
+     * {@link #shouldTrigger()}.
+     * 
+     * <p>
+     * Note: {@link #shouldTrigger()} is not invoked, which may be significant
+     * if it has side-effects
+     */
     public final void forcePerform() {
         doPerform();
+    }
+
+    /**
+     * Actually performs the action. Subclasses may override it to perform
+     * additional bookkeeping, but should call superclass' {@link #doPerform()}.
+     */
+    protected void doPerform() {
+        action.run();
     }
 
 }
--- GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/model/FixedRateActionFilter.java
+++ GriegCore/src/main/java/pl/edu/agh/ki/grieg/processing/model/FixedRateActionFilter.java
@@ -2,26 +2,64 @@ package pl.edu.agh.ki.grieg.processing.model;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * {@link ActionFilter} implementation that allows for triggering action only
+ * after the specified amount of time since last invocation has passed.
+ * 
+ * @author los
+ */
 public class FixedRateActionFilter extends ActionFilter {
 
+    /** Time between consecutive invocations in nanoseconds */
     private final long delta;
 
+    /**
+     * Amount of nanoseconds since some arbitrary point in time when the action
+     * was fired last time
+     */
     private long lastFired;
 
-    protected FixedRateActionFilter(TimeUnit unit, long delta) {
+    /**
+     * Creates new {@link FixedRateActionFilter} with no action and the
+     * specified delay between invocations.
+     * 
+     * @param unit
+     *            Time unit
+     * @param delta
+     *            Amount of time
+     */
+    public FixedRateActionFilter(TimeUnit unit, long delta) {
         this.delta = unit.toNanos(delta);
     }
 
-    protected FixedRateActionFilter(Runnable action, TimeUnit unit, long delta) {
+    /**
+     * Creates new {@link FixedRateActionFilter} with specified action delay
+     * between invocations.
+     * 
+     * @param unit
+     *            Time unit
+     * @param delta
+     *            Amount of time
+     */
+    public FixedRateActionFilter(Runnable action, TimeUnit unit, long delta) {
         super(action);
         this.delta = unit.toNanos(delta);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected boolean shouldTrigger() {
         return System.nanoTime() - lastFired > delta;
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * <p>
+     * Updates last-invocation-time.
+     */
     @Override
     protected void doPerform() {
         super.doPerform();
