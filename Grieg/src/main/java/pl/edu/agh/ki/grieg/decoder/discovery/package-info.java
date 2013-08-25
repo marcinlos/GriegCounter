c53a0296246f65b17c56d2c2c266df5ba73378da
los <losiu99@gazeta.pl>
2013-08-26 00:19:23 +0200

    Added some documentation to parser discovery module's package-info.java

--- Grieg/src/main/java/pl/edu/agh/ki/grieg/decoder/discovery/package-info.java
+++ Grieg/src/main/java/pl/edu/agh/ki/grieg/decoder/discovery/package-info.java
@@ -2,6 +2,16 @@
  * This package contains implementation of custom SPI mechanism used to find
  * audio file parsers.
  * 
+ * <p>
+ * During initialization, file matching the classpath-relative path {@c
+ * pl.edu.agh.ki.grieg/parsers} are found using specified classloader (thread
+ * context classloader by default), and parsed, to obtain audio parser
+ * definitions. Parser definition consists of class implementing
+ * {@link pl.edu.agh.ki.grieg.decoder.spi.AudioFormatParser}, and list of 
+ * supported extensions. Classes found in this way are instantiated using 
+ * {@link java.lang.Class#newInstance()}.
+ * 
  * @author los
  */
 package pl.edu.agh.ki.grieg.decoder.discovery;
+
