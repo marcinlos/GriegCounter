1fdf8a954b01cddfb7630e0ff2032f9b60aa56cc
los <losiu99@gazeta.pl>
2013-11-08 15:12:07 +0100

    Removed unused swing gui parts

--- GriegSwing/src/pl/edu/agh/ki/grieg/swing/graphics/transform/Transform.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.ki.grieg.swing.graphics.transform;
-
-import pl.edu.agh.ki.grieg.swing.graphics.Point;
-
-public interface Transform {
-    
-    Point apply(Point p);
-
-}
--- GriegSwing/src/pl/edu/agh/ki/grieg/swing/graphics/transform/Transformable.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.ki.grieg.swing.graphics.transform;
-
-public interface Transformable {
-
-    void push(Transform t);
-    Transform pop();
-    void clear();
-    
-}
