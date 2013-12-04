7a3bb6e3bcd9f18e9d102581588c66c3f16f8977
los <losiu99@gazeta.pl>
2013-12-04 21:06:34 +0100

    Removed old Android stub

--- GUI/src/com/grieg/interfaces/WaveCompressor.java
+++
@@ -1,6 +0,0 @@
-package com.grieg.interfaces;
-
-public interface WaveCompressor {
-	
-
-}
--- GUI/src/com/grieg/listeners/SpectrumListener.java
+++
@@ -1,7 +0,0 @@
-package com.grieg.listeners;
-
-public interface SpectrumListener {
-	
-	public void getPixelForSpectrum(float min, float max);
-
-}
