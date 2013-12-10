c9f90c307812587e5451c37ddc6312fc9b2a2021
Deltharis <michal.torba@gmail.com>
2013-12-11 00:58:39 +0100

    Tabhost for both cases and metadata finally working (~again). Added new method for propertiesHelper

--- AndroidDemo/src/pl/edu/agh/ki/grieg/android/demo/DefaultBActivity.java
+++ AndroidDemo/src/pl/edu/agh/ki/grieg/android/demo/DefaultBActivity.java
@@ -1,8 +1,10 @@
 package pl.edu.agh.ki.grieg.android.demo;
 
+import java.awt.TextField;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -19,6 +21,7 @@ import pl.edu.agh.ki.grieg.model.CompositeModel;
 import pl.edu.agh.ki.grieg.model.Model;
 import pl.edu.agh.ki.grieg.model.Models;
 import pl.edu.agh.ki.grieg.processing.core.ProcessingAdapter;
+import pl.edu.agh.ki.grieg.processing.core.ProcessingListener;
 import pl.edu.agh.ki.grieg.processing.core.Processor;
 import pl.edu.agh.ki.grieg.processing.core.ProcessorFactory;
 import pl.edu.agh.ki.grieg.processing.model.AudioModel;
@@ -28,10 +31,13 @@ import pl.edu.agh.ki.grieg.processing.model.WaveFunctionModel;
 import pl.edu.agh.ki.grieg.processing.model.WaveWindowModel;
 import pl.edu.agh.ki.grieg.processing.pipeline.Pipeline;
 import pl.edu.agh.ki.grieg.util.ProgressListener;
+import pl.edu.agh.ki.grieg.util.PropertiesHelper;
 import pl.edu.agh.ki.grieg.util.Reflection;
 import pl.edu.agh.ki.grieg.util.iteratee.Iteratee;
 import pl.edu.agh.ki.grieg.util.math.Point;
+import pl.edu.agh.ki.grieg.util.properties.Properties;
 import roboguice.activity.RoboActivity;
+import roboguice.activity.RoboTabActivity;
 import roboguice.inject.InjectView;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -43,6 +49,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 public class DefaultBActivity extends RoboActivity {
 
@@ -77,6 +84,11 @@ public class DefaultBActivity extends RoboActivity {
 	@InjectView(R.id.spectrum)
 	private SpectrumView spectrum;
 	
+	@InjectView(R.id.Metadata)
+	private TextView tx;
+	
+	private TabHost tabHost;
+
 	ProgressDialog pd;
 
 	WaveWindowModel waveWindow;
@@ -98,43 +110,14 @@ public class DefaultBActivity extends RoboActivity {
 		onlyOffline = sharedPreferences.getBoolean(
 				Dictionary.ONLY_OFFLINE.getCaption(), false);
 		// TABS//
-		TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
-		tabHost.setup();
 
-		TabSpec wave = tabHost.newTabSpec("Tab 1");
-		wave.setContent(R.id.tab1);
-		wave.setIndicator("Tab 1");
-
-		TabSpec spectral = tabHost.newTabSpec("Tab 3");
-		spectral.setIndicator("Tab 3");
-		spectral.setContent(R.id.tab2);
-
-		TabSpec power = tabHost.newTabSpec("Tab 2");
-		power.setIndicator("Tab 2");
-		power.setContent(R.id.tab4);
-
-		TabSpec window = tabHost.newTabSpec("Tab 4");
-		window.setIndicator("Tab 4");
-		window.setContent(R.id.tab5);
-
-		tabHost.setCurrentTab(0);
-		if(onlyOffline){
+		if (onlyOffline) {
 			factory = getGrieg().getOfflineFactory();
-		}
-		else{
+		} else {
 			factory = getGrieg().getFullFactory();
 		}
-		
-		
-		//Progress bar!
-				pd = new ProgressDialog(this);
-				pd.setTitle("Processing...");
-				pd.setMessage("Please wait.");
-				pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-				pd.setCancelable(false);
-				pd.setIndeterminate(false);
-				pd.show();
-		//END progress bar
+		createTabs();
+		progressBar();
 
 		modelRoot = Models.container();
 		createModels();
@@ -142,19 +125,16 @@ public class DefaultBActivity extends RoboActivity {
 
 		waveLeftChannel.setModel(modelRoot.getChild("wave.left", clazz));
 		waveRightChannel.setModel(modelRoot.getChild("wave.right", clazz));
-		tabHost.addTab(wave);
+
 		power_left.setModel(modelRoot.getChild("power.left", clazz));
 		power_left.setZeroToOneScale();
 		power_right.setModel(modelRoot.getChild("power.right", clazz));
 		power_right.setZeroToOneScale();
-		tabHost.addTab(power);
 
 		if (!onlyOffline) {
 			narrow.setModel(modelRoot.getChild("window.narrow.left", clazz));
 			wide.setModel(modelRoot.getChild("window.wide.left", clazz));
-			tabHost.addTab(spectral);
 			spectrum.setModel(modelRoot.getChild("fft.power", float[].class));
-			tabHost.addTab(window);
 		}
 
 		try {
@@ -163,6 +143,54 @@ public class DefaultBActivity extends RoboActivity {
 			logger.error("Error", e);
 		}
 
+	}
+
+	private void createTabs() {
+
+		tabHost = (TabHost) findViewById(android.R.id.tabhost);
+		tabHost.setup();
+		TabSpec wave = tabHost.newTabSpec("Wave");
+		wave.setContent(R.id.tabWave);
+		wave.setIndicator("Wave");
+		tabHost.addTab(wave);
+
+		TabSpec power = tabHost.newTabSpec("Power");
+		power.setIndicator("Power");
+		power.setContent(R.id.tabPower);
+		tabHost.addTab(power);
+
+		TabSpec spectral = tabHost.newTabSpec("Spectrum");
+		spectral.setIndicator("Spectrum");
+		spectral.setContent(R.id.tabSpectrum);
+		tabHost.addTab(spectral);
+
+		TabSpec window = tabHost.newTabSpec("WaveWin");
+		window.setIndicator("WaveWin");
+		window.setContent(R.id.tabWaveWindow);
+		tabHost.addTab(window);
+
+		TabSpec metadata = tabHost.newTabSpec("Meta");
+		metadata.setIndicator("Meta");
+		metadata.setContent(R.id.tabMeta);
+		tabHost.addTab(metadata);
+
+		if (onlyOffline) {
+			tabHost.getTabWidget().getChildTabViewAt(2)
+					.setVisibility(View.GONE);
+			tabHost.getTabWidget().getChildTabViewAt(3)
+					.setVisibility(View.GONE);
+		}
+
+	}
+	
+	private void progressBar(){
+		pd = new ProgressDialog(this);
+		pd.setTitle("Processing...");
+		pd.setMessage("Please wait.");
+		pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		pd.setCancelable(false);
+		pd.setIndeterminate(false);
+		pd.show();
 	}
 
 	private void createModels() {
@@ -214,34 +242,45 @@ public class DefaultBActivity extends RoboActivity {
 				ctx.addFeaturesListener(extractionModel);
 				ctx.addProgressListener(extractionModel);
 				ctx.addProgressListener(new ProgressListener() {
-					
+
 					@Override
 					public void started() {
 						pd.setProgress(0);
-						
+
 					}
-					
+
 					@Override
 					public void progress(float percent) {
-						//logger.debug(String.valueOf(percent));
-						pd.setProgress((int)(percent*100));
-						
-						
+						// logger.debug(String.valueOf(percent));
+						pd.setProgress((int) (percent * 100));
+
 					}
-					
+
 					@Override
 					public void finished() {
 						logger.debug("pre finished");
 						pd.dismiss();
-						
+
 					}
-					
+
 					@Override
 					public void failed(Exception e) {
 						logger.debug(e.getLocalizedMessage());
-						
+
 					}
 				});
+			}
+
+			@Override
+			public void afterPreAnalysis(Properties results) {
+				tx.setText("Well...");
+				String metadata = "";
+				/*for (Entry<String, Object> entry : results.entrySet()) {
+					String key = entry.getKey();
+					Object value = entry.getValue();
+					metadata = metadata + key + ": " + value + "\n";
+				}*/
+				tx.setText(PropertiesHelper.dumpWithoutTypes(results));
 			}
 		});
 
@@ -273,8 +312,7 @@ public class DefaultBActivity extends RoboActivity {
 				logger.info("Metadata gathered");
 			} catch (Exception e) {
 				logger.error("Error during preliminary analysis", e);
-			}
-			catch(Throwable t){
+			} catch (Throwable t) {
 				logger.error("zlee");
 				logger.error("zlee", t);
 			}
@@ -292,13 +330,12 @@ public class DefaultBActivity extends RoboActivity {
 		public void run() {
 			try {
 				logger.info("Beginning audio analysis");
-					proc.analyze();
-				
+				proc.analyze();
+
 				logger.info("Audio analysis finished");
 			} catch (Exception e) {
 				logger.error("Error during the main analysis phase", e);
-			}
-			catch(Throwable t){
+			} catch (Throwable t) {
 				logger.error("zlee");
 				logger.error("zlee", t);
 			}
@@ -307,12 +344,11 @@ public class DefaultBActivity extends RoboActivity {
 
 	private void startProcessing() throws AudioException, IOException {
 		final Processor proc = factory.newFileProcessor(new File(file_chosen));
-		try{
+		try {
 			proc.openFile();
 			enqueue(new PreAnalysis(proc));
 			enqueue(new Analysis(proc));
-		}
-		catch(Throwable t){
+		} catch (Throwable t) {
 			logger.error("zleee");
 			logger.error("here", t);
 		}
@@ -332,18 +368,17 @@ public class DefaultBActivity extends RoboActivity {
 		super.onBackPressed();
 		this.finish();
 	}
+
 	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event)
-	{
-	    if ((keyCode == KeyEvent.KEYCODE_BACK))
-	    {
-	        finish();
-	    }
-	    return super.onKeyDown(keyCode, event);
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+			finish();
+		}
+		return super.onKeyDown(keyCode, event);
 	}
-	
+
 	@Override
-	public void onDestroy(){
+	public void onDestroy() {
 		android.os.Process.killProcess(android.os.Process.myPid());
 		super.onDestroy();
 	}
--- GriegCore/src/main/java/pl/edu/agh/ki/grieg/util/PropertiesHelper.java
+++ GriegCore/src/main/java/pl/edu/agh/ki/grieg/util/PropertiesHelper.java
@@ -31,6 +31,26 @@ public final class PropertiesHelper {
         return sb.toString();
     }
     
+    public static String dumpWithoutTypes(Properties properties) {
+    	StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        if (properties.isEmpty()) {
+            return "";
+        }
+        for (Entry<String, ?> entry : properties.entrySet()) {
+            String key = entry.getKey();
+            Object val = entry.getValue();
+            if (first) {
+                first = false;
+            } else {
+                sb.append('\n');
+            }
+            sb.append("    ")
+              .append(formatPair(key, val));
+        }
+        return sb.toString();
+    }
+    
     private static String formatPair(String key, Object val) {
         return String.format("%-15s -> %s", key, val);
     }
